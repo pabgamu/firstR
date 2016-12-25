d675b53315fd24d5c2e74a08c220fac31ed4d528
Pablo Garcia <pabgamu@gmail.com>
2016-12-25 14:16:00 +0100

    setting load data path so vignette works

--- R/functions.R
+++ R/functions.R
@@ -47,7 +47,7 @@ fars_read <- function(filename) {
 #'
 make_filename <- function(year) {
         year <- as.integer(year)
-        sprintf("accident_%d.csv.bz2", year)
+        file.path('..','inst','extdata', sprintf("accident_%d.csv.bz2", year))
 }
 
 
